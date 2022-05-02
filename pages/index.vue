06ae22adbb93768f0c1878ece6fd8a1b5dd61d55
CShingiro <nemyent@gmail.com>
2022-05-02 09:28:06 -0400

    Configuring first API call

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,33 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCardVue
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+      />
   </div>
 </template>
 
 <script>
+import EventCardVue from '~/components/EventCard.vue'
 export default {
   name: 'IndexPage',
   head () {
     return {
       title: 'Event Listing'
     }
+  },
+  asyncData ({ $axios }) {
+    return $axios.get('http://localhost:3000/events').then(response => {
+      return {
+        events: response.data
+      }
+    })
+  },
+  components: {
+    EventCardVue
   }
 }
 </script>
